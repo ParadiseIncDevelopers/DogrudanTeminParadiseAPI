2102f6bce37257026da05b5c31e56e745d59418b
ParadiseIncDevelopers <95352070+ParadiseIncDevelopers@users.noreply.github.com>
2025-07-06 22:48:47 +0300

    Add bulk insert endpoints and services

--- DogrudanTeminParadiseAPI/Service/Abstract/ICategoryService.cs
+++ DogrudanTeminParadiseAPI/Service/Abstract/ICategoryService.cs
@@ -5,6 +5,7 @@ namespace DogrudanTeminParadiseAPI.Service.Abstract
     public interface ICategoryService
     {
         Task<CategoryDto> CreateAsync(CreateCategoryDto dto);
+        Task<IEnumerable<CategoryDto>> AddMassAsync(List<CreateCategoryDto> dtos);
         Task<IEnumerable<CategoryDto>> GetAllAsync();
         Task<CategoryDto> GetByIdAsync(Guid id);
         Task<CategoryDto> UpdateAsync(Guid id, UpdateCategoryDto dto);
