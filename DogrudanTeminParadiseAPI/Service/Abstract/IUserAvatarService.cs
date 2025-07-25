462261b80a8ad9e7c9cb26eba1a9c4cbad6a2353
ParadiseIncDevelopers <95352070+ParadiseIncDevelopers@users.noreply.github.com>
2025-07-25 12:39:06 +0300

    Add endpoint to list all user avatars

---
+++ DogrudanTeminParadiseAPI/Service/Abstract/IUserAvatarService.cs
@@ -0,0 +1,12 @@
+using DogrudanTeminParadiseAPI.Dto;
+
+namespace DogrudanTeminParadiseAPI.Service.Abstract
+{
+    public interface IUserAvatarService
+    {
+        Task<UserAvatarDto> CreateAsync(CreateUserAvatarDto dto);
+        Task<IEnumerable<UserAvatarDto>> GetAllAsync();
+        Task<UserAvatarDto?> GetByUserOrAdminIdAsync(Guid userOrAdminId);
+        Task<UserAvatarDto?> UpdateAsync(Guid userOrAdminId, UpdateUserAvatarDto dto);
+    }
+}
